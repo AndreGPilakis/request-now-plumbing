6e73ab66bf9490cac629c13f547423b7bc529ec8
Andre Pilakis <andregpilakis@gmail.com>
2020-02-18 10:42:57 +0000

    slowly porting gallery to grid system with modals

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log(
+    "loaded"
+);
+// $('#myModal').modal('toggle')
+function showModal(modalNumber){
+    console.log("Showing modal " + modalNumber);
+    $('#modal-1').modal('toggle')
+}
